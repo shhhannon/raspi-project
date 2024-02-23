b6fec239df8361879c6750b70e7b4636aca4403b
pigeoncoder1 <richyrayc@gmail.com>
2024-02-23 14:39:34 +0000

    23/02/2024

--- assid/InhalerAid/views.py
+++ assid/InhalerAid/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     return render(request, "welcome.html")
+
+def receive_data_view(request):
+    if request.method == 'POST':
+        # Process the incoming data
+        received_data = request.POST.get('data', None)
+        # Process the data as needed
+        print(received_data)
+        return render(request, "data.html")
