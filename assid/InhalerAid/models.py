0f54f240b9fdd213a2fda1ab8df7e9d5e5b08fe4
shhhannon <159655816+shhhannon@users.noreply.github.com>
2024-03-13 16:57:11 +0000

    Update models.py

--- assid/InhalerAid/models.py
+++ assid/InhalerAid/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class reading(models.Model):
   airQuality = models.CharField(max_length = 20)
   dateTime = models.DateTimeField()
-  numDoses = models.IntergerField()
+  numDoses = models.IntegerField()
