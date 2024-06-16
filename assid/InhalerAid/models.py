76b3067f6e4c74dcd2fe34bc7faae5b897af85f1
shhhannon <159655816+shhhannon@users.noreply.github.com>
2024-06-16 20:29:04 +0100

    Update models.py

--- assid/InhalerAid/models.py
+++ assid/InhalerAid/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-class sensorData(models.Model):
+class readings(models.Model):
   date = models.DateField()
   time = models.TimeField()
-  eco2Levels = models.FloatField()
-  totalVoc = models.FloatField()
+  CO2 = models.FloatField()
   doses = models.IntegerField(default=200)
 
   def __str__(self):
-        return f"{self.date} {self.time} - eCO2: {self.eco2_levels} ppm, VOC: {self.total_voc} ppb"
+        return f"{self.date} {self.time} - eCO2: {self.eco2_levels} ppm"
