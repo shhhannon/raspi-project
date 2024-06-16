9a937ea4b0f07cf78bd30273cd68db7677ab281b
shhhannon <159655816+shhhannon@users.noreply.github.com>
2024-06-16 20:29:46 +0100

    Update views.py

--- assid/InhalerAid/views.py
+++ assid/InhalerAid/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
+from .models import readings
 
-# Create your views here.
 def welcome(request):
-    return render(request, "welcome.html")
+    #Get readings from database
+    all_data = readings.objects.all()
+    
+    #Pass this data to the template
+    context = {
+        'readings':all_data
+    }
 
-def receive_data_view(request):
-    if request.method == 'POST':
-        # Process the incoming data
-        received_data = request.POST.get('data', None)
-        # Process the data as needed
-        print(received_data)
-        reading = reading(airQuality =  received_data[co2Quality_data], dateTime = received_data[dateTime_data], numDoses = receieved_data[numDoses_data])
-        readings = model.objects.get(reading)
-                          
-        return render(request, "data.html", context={'readings':readings})
+    return render(request, "welcome.html", context)
+
