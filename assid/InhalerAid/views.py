0e1d602b071f9caf88005b15b75f8930abc16ba2
shhhannon <159655816+shhhannon@users.noreply.github.com>
2024-06-17 00:22:18 +0100

    Update views.py

--- assid/InhalerAid/views.py
+++ assid/InhalerAid/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
-from .models import readings
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from InhalerAid.models import readings
+from datetime import datetime
 
-def welcome(request):
-    #Get readings from database
-    all_data = readings.objects.all()
-    
-    #Pass this data to the template
-    context = {
-        'readings':all_data
-    }
+@csrf_exempt
+def save_reading(request):
+    if request.method == 'POST':
+        co2 = float(request.POST.get('CO2'))
+        date_time = datetime.fromisoformat(request.POST.get('dateTime'))
+        doses = readings.objects.last().doses - 2 if readings.objects.exists() else 200
+        readings.objects.create(
+            date=date_time.date(),
+            time=date_time.time(),
+            co2=co2,
+            doses=doses
+        )
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'failure', 'reason': 'Invalid method'}, status=405)
 
-    return render(request, "welcome.html", context)
+def readings_table(request):
+    all_readings = readings.objects.all()
+    return render(request, 'data.html', {'readings': readings})
+
 
